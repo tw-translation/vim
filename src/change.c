d9dafe48ba08cb9bcd1876314da083838538f365
pan93412 <pan93412@gmail.com>
2019-10-07 19:30:54 +0800

    fix(i18n): error: initializer element is not constant

--- src/change.c
+++ src/change.c
@@ -26,7 +26,8 @@
     void
 change_warning(int col)
 {
-    static char *w_readonly = wrnmsg(10, N_("Changing a readonly file"));
+    static char *w_readonly;
+    w_readonly = wrnmsg(10, N_("Changing a readonly file"));
 
     if (curbuf->b_did_warn == FALSE
 	    && curbufIsChanged() == 0
